438384372f47a4ac0307901db3d116f759292fe7
Ud-Bawangade <ud.bawangade@gmail.com>
2022-12-24 09:51:20 +0530

    new syllabus added

---
+++ sample1.java
@@ -0,0 +1,15 @@
+# What is VCS. Why we need VCS.
+	VCS features.
+
+#
+GIT Installation (Ubuntu):
+$ sudo apt-get update
+$ sudo apt-get install git
+
+Verify Installation:
+	which git
+	git --version
+
+
+GIT Uninstallation:
+$ sudo apt-get remove git
